8e4863f9502ff8222263e2e2e9a2434dbd3c2925
Ahmad.Nawawi <ahmad.nawawi@jo.zain.com>
2024-02-13 12:27:19 +0300

    Removed connection properties from config class

--- order-service/src/main/java/dev/awn/orderservice/common/config/KafkaProducerConfig.java
+++ order-service/src/main/java/dev/awn/orderservice/common/config/KafkaProducerConfig.java
@@ -1,34 +1,15 @@
 package dev.awn.orderservice.common.config;
 
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
-    private final String bootstrapServers = "localhost:9092";
 
     @Bean
-    public ProducerFactory<String, String> producerFactory() {
-        Map<String, Object> configs = new HashMap<>();
-
-        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
-        return new DefaultKafkaProducerFactory<>(configs);
-    }
-
-    @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
     }
 }
